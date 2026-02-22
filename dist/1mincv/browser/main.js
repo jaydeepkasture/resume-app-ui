5bb622817874592ae71a8bed89284c416a607b76
Jaydeep Kasture <jaydeepkastaure7@gmail.com>
2026-02-22 13:22:59 +0530

    build(fix(right side editor margin))

--- dist/1mincv/browser/main.js
+++ dist/1mincv/browser/main.js
@@ -2168,12 +2168,12 @@ var routes = [
   // Protected routes (require authentication)
   {
     path: "editor",
-    loadComponent: () => import("./chunk-DG4P3JEK.js").then((m) => m.ResumeEditorComponent),
+    loadComponent: () => import("./chunk-DBG2C6P4.js").then((m) => m.ResumeEditorComponent),
     canActivate: [AuthGuard]
   },
   {
     path: "editor/:chatId",
-    loadComponent: () => import("./chunk-DG4P3JEK.js").then((m) => m.ResumeEditorComponent),
+    loadComponent: () => import("./chunk-DBG2C6P4.js").then((m) => m.ResumeEditorComponent),
     canActivate: [AuthGuard]
   },
   {
